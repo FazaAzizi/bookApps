d6e6765a79e068e52a078126a9fe9f1cf1ee2470
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-05 10:40:21 +0700

    homepage2

---
+++ app/src/main/java/com/android/bookapps/modelstigma1.java
@@ -0,0 +1,17 @@
+package com.android.bookapps;
+
+public class modelstigma1 {
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public modelstigma1(String desc) {
+        this.desc = desc;
+    }
+}
