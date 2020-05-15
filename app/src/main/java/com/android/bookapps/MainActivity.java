db073af4416a5fda887e1a441ad46b428fb56c1b
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-15 16:11:35 +0700

    new1

--- app/src/main/java/com/android/bookapps/MainActivity.java
+++ app/src/main/java/com/android/bookapps/MainActivity.java
@@ -3,7 +3,9 @@ package com.android.bookapps;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
 
 import java.util.ArrayList;
@@ -16,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+    }
+
+    public void Subtema1(View view){
+        Intent intent = new Intent(MainActivity.this, SubTema1Activity.class);
+        startActivity(intent);
     }
 }
