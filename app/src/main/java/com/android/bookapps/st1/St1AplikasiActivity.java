afe39a732a78ed93586418a636274363f39e038f
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-16 14:49:03 +0700

    st1.3

---
+++ app/src/main/java/com/android/bookapps/st1/St1AplikasiActivity.java
@@ -0,0 +1,16 @@
+package com.android.bookapps.st1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.bookapps.R;
+
+public class St1AplikasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st1_aplikasi);
+    }
+}
