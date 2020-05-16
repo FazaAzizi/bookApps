7aed5dc5a8092e72f87cb54e24edb3765d2cb1cb
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-16 12:23:21 +0700

    st1pendahuluan

--- app/src/main/java/com/android/bookapps/SubTema1Activity.java
+++ app/src/main/java/com/android/bookapps/SubTema1Activity.java
@@ -2,7 +2,11 @@ package com.android.bookapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.android.bookapps.st1.St1PendahuluanActivity;
 
 public class SubTema1Activity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class SubTema1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_tema1);
+    }
+
+    public void st1pendahuluanmtd(View view){
+        Intent intent = new Intent(SubTema1Activity.this, St1PendahuluanActivity.class);
+        startActivity(intent);
     }
 }
