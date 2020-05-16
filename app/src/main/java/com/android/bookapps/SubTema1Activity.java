923ada71b970d3d9af787a8eaf658f3dbc199226
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-16 14:07:52 +0700

    st1.2

--- app/src/main/java/com/android/bookapps/SubTema1Activity.java
+++ app/src/main/java/com/android/bookapps/SubTema1Activity.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.bookapps.st1.St1AplikasiActivity;
+import com.android.bookapps.st1.St1PemantapanActivity;
 import com.android.bookapps.st1.St1PendahuluanActivity;
+import com.android.bookapps.st1.St1PengembanganActivity;
+import com.android.bookapps.st1.St1PenilaianActivity;
 import com.android.bookapps.st1.St1RangkumanActivity;
 
 public class SubTema1Activity extends AppCompatActivity {
@@ -23,6 +27,22 @@ public class SubTema1Activity extends AppCompatActivity {
     }
     public void st1rangkumanmtd(View view){
         Intent intent = new Intent(SubTema1Activity.this, St1RangkumanActivity.class);
+        startActivity(intent);
+    }
+    public void st1pengembanganmtd(View view){
+        Intent intent = new Intent(SubTema1Activity.this, St1PengembanganActivity.class);
+        startActivity(intent);
+    }
+    public void st1aplikasimtd(View view){
+        Intent intent = new Intent(SubTema1Activity.this, St1AplikasiActivity.class);
+        startActivity(intent);
+    }
+    public void st1pemantapanmtd(View view){
+        Intent intent = new Intent(SubTema1Activity.this, St1PemantapanActivity.class);
+        startActivity(intent);
+    }
+    public void st1penilaianmtd(View view){
+        Intent intent = new Intent(SubTema1Activity.this, St1PenilaianActivity.class);
         startActivity(intent);
     }
 }
