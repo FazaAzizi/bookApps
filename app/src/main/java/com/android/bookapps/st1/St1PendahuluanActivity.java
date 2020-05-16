91cdf6c3edafc053e20aa5fafc2a440ea6aa5875
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-16 12:07:43 +0700

    st1

---
+++ app/src/main/java/com/android/bookapps/st1/St1PendahuluanActivity.java
@@ -0,0 +1,14 @@
+package com.android.bookapps.st1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class St1PendahuluanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st1_pendahuluan);
+    }
+}
