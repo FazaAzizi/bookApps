ed1e43e18035e6017ee9d21993966ba87d67b2d8
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-19 13:49:34 +0700

    brgr

--- app/src/main/java/com/android/bookapps/MainActivity.java
+++ app/src/main/java/com/android/bookapps/MainActivity.java
@@ -1,27 +1,91 @@
 package com.android.bookapps;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 
 import com.android.bookapps.st1.St1PendahuluanActivity;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(" ");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                Activity selectedActivity = null;
+                switch(id)
+                {
+                    case R.id.login:
+                        break;
+//                    case R.id.about:
+//                        about();
+//                        break;
+//                    case R.id.help:
+//                        help();
+//                        break;
+                    case R.id.tips1:
+                        break;
+                    case R.id.gym1:
+                        break;
+                    case R.id.trainer1:
+                        break;
+                    case R.id.cp1:
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void Subtema1(View view){
