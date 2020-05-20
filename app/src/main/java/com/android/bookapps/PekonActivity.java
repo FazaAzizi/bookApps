903d3f5af1691261b2ef69b78968b54dc0a73939
fazaazizi <faza.azizi.faz@gmail.com>
2020-05-21 00:53:08 +0700

    kzl

---
+++ app/src/main/java/com/android/bookapps/PekonActivity.java
@@ -0,0 +1,14 @@
+package com.android.bookapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PekonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pekon);
+    }
+}
